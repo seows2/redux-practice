31be36f13c5429f1a92416a9e38c07dff3c89768
SeoWS <seows2@naver.com>
2021-01-04 00:26:23 +0900

    기본 코드 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Counter from "./components/Counter/Counter"
+import Palette from './components/Palette/Palette';
+import WaitingList from './components/WaitingList/WaitingList';
 
-function App() {
+function App(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Palette selected="red" />
+      <Counter value={0} color="red" />
+      <WaitingList />
     </div>
   );
 }
